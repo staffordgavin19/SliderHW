cf886d8fe3dd7ce0ac4609445c0a0fa24ff7acef
Gavin Stafford <gavin.stafford@student.swic.edu>
2021-04-20 22:03:05 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
+const p = document.querySelector("p");
+const texter = document.querySelector("#txt");
+const sizer = document.querySelector("#font-size");
+const colorer = document.querySelector("#color");
+const root = document.querySelector(":root");
 
+texter.addEventListener("keyup", (event) => {
+  p.innerText = event.target.value;
+});
+
+sizer.addEventListener("input", (event) => {
+  root.style.setProperty("--font-size", `${event.target.value}px`);
+});
+
+colorer.addEventListener("input", (event) => {
+  root.style.setProperty("--color", event.target.value);
+});
